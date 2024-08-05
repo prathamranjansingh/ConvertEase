a4673410be52145f725eda2f1fa94f8d8a137261
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-08-06 00:15:22 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const path = require("path");
 const imageRoutes = require("./routes/imageRoutes");
-
-dotenv.config();
 
 const app = express();
 
@@ -15,7 +12,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/images", imageRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
